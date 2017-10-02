4f86d6a0c2efbe058b235e81ce9d08523c7c0e0a
FaizeChan <obey.faize@gmail.com>
2017-10-02 15:16:28 -0400

    all these arrays

--- Week2_day1School/Program.cs
+++ Week2_day1School/Program.cs
@@ -10,16 +10,79 @@ namespace Week2_day1School
     {
         static void Main(string[] args)
         {
-
+            //arrays never change ever
             //int favoriteNumber;
-            int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6 };
+            //int[] favoriteNumbers = { 1, 2, 3, 4, 5, 6 };
 
-            string[] studentNames = { "Faizon", "Raz", "Monica", "Brian", "Ali", "David" };
+            //string[] studentNames = { "Faizon", "Raz", "Monica", "Brian", "Ali", "David" };
 
-            Console.WriteLine(favoriteNumbers[0]);
-            Console.WriteLine(studentNames[2]);
+            //Console.WriteLine(favoriteNumbers[0]);
+            //Console.WriteLine(studentNames[2]);
 
-            int[] numbers = new int[4];
+            //int[] numbers = new int[4];
+            //numbers[0] = 43;
+            //numbers[1] = 12;
+            //Console.WriteLine("Please enter a number");
+            //numbers[2] = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter a number");
+            //numbers[3] = int.Parse(Console.ReadLine());
+
+            //numbers[0] = 30;
+
+            //Console.WriteLine(numbers[3]);
+
+
+            //string[] daysOfWeek = new string[7];
+            //daysOfWeek[0] = "Sun";
+            //daysOfWeek[1] = "Mon";
+            //daysOfWeek[2] = "Tues";
+            //daysOfWeek[3] = "Wed";
+            //daysOfWeek[4] = "Thurs";
+            //daysOfWeek[5] = "Fri";
+            //daysOfWeek[6] = "Sat";
+
+            //int[] studentAges = new int[4];
+            //studentAges[0] = 7;
+            //studentAges[1] = 3;
+            //studentAges[2] = 14;
+            //studentAges[3] = 10;
+
+
+            //Console.WriteLine(daysOfWeek.Length);
+            //Console.WriteLine(studentAges[2]);
+
+
+            //string[] userName = new string[7];
+            //userName[0] = "a";
+            //userName[1] = "l";
+            //userName[2] = "e";
+            //userName[3] = "x";
+            //userName[4] = "n";
+            //userName[5] = "d";
+            //userName[6] = "r";
+
+            //Console.Write(userName[0]);
+            //Console.Write(userName[1]);
+            //Console.Write(userName[2]);
+            //Console.Write(userName[3]);
+            //Console.Write(userName[0]);
+            //Console.Write(userName[4]);
+            //Console.Write(userName[5]);
+            //Console.Write(userName[2]);
+            //Console.Write(userName[6]);
+            //Console.WriteLine("\n");
+
+            //char[] lastName = { 'A', 'l', 'e', 'x', 'a', 'n', 'd', 'e', 'r' };
+            //Console.WriteLine(lastName.Length);
+
+            string[] exampleStringArray = { "This", "is", "an", "array", "of", "words." };
+            Console.WriteLine(exampleStringArray[exampleStringArray.Length - 1]);
+
+            //int index = exampleStringArray.Length;
+            //index = index - 2;
+
+            //Console.WriteLine("This is the last index " + (exampleStringArray.Length - 1));
+
 
             //string[] vacationSpots = {"Tokeyo", "Hawaii", "California", "Las Vegas", "Home", "DeathValley", "Disney World", "Paris", "Cruising in the Pacific", "long Island Beach" };
             //int[] childrensAges = { 7, 5, 3 };
